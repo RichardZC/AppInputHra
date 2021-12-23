309919b5844c220201580a78a0e504be2f08372b
Richard Zapata <richard.zapata.c@gmail.com>
2021-12-23 09:25:07 -0500

    login

---
+++ Hra.Input/Hra.App/Controllers/SeguridadController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Hra.App.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Autenticar()
+        {
+            if (true)
+            {
+                List<Claim> claims = new List<Claim> {
+                    new Claim(ClaimTypes.NameIdentifier, 1.ToString())
+                };
+                var identity = new ClaimsIdentity(claims, "Hra");
+                var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                await HttpContext.SignInAsync(userPrincipal);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Seguridad");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+            return RedirectToAction("Index", "Seguridad");
+        }
+    }
+}
--- Hra.Input/Hra.Input/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hra.Input.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
