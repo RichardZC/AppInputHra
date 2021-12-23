d45b5d5def3cc7d733b63d187f11b87ae0df316e
Richard Zapata <richard.zapata.c@gmail.com>
2021-12-23 15:55:10 -0500

    Adhra

--- Hra.Input/Hra.App/Controllers/HomeController.cs
+++ Hra.Input/Hra.App/Controllers/HomeController.cs
@@ -19,11 +19,7 @@ namespace Hra.App.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+                
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Hra.Input/Hra.App/Controllers/SeguridadController.cs
+++ Hra.Input/Hra.App/Controllers/SeguridadController.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using System.DirectoryServices;
 using System.Security.Claims;
 
 namespace Hra.App.Controllers
 {
     public class SeguridadController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string mensaje="")
         {
             if (User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
             }
+            ViewBag.Mensaje= mensaje;
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> Autenticar()
+        public async Task<IActionResult> Autenticar(string pUsuario, string pClave)
         {
-            if (true)
+            string path = @"LDAP://adhra.hrayacucho.gob.pe";       //CAMBIAR POR VUESTRO PATH (URL DEL SERVICIO DE DIRECTORIO LDAP)
+            string dominio = @"adhra";             //CAMBIAR POR VUESTRO DOMINIO
+            string usu = pUsuario;                   //USUARIO DEL DOMINIO
+            string pass = pClave;                    //PASSWORD DEL USUARIO
+            string domUsu = dominio + @"\" + usu;               //CADENA DE DOMINIO + USUARIO A COMPROBAR
+
+            bool permiso = AutenticaUsuario(path, domUsu, pass);
+            
+            if (permiso)
             {
                 List<Claim> claims = new List<Claim> {
                     new Claim(ClaimTypes.NameIdentifier, 1.ToString())
@@ -29,10 +40,29 @@ namespace Hra.App.Controllers
             }
             else
             {
-                return RedirectToAction("Index", "Seguridad");
+                return RedirectToAction("Index", "Seguridad", new { mensaje = "Credenciales Incorrecta!" });
             }
 
             return RedirectToAction("Index", "Home");
+        }
+        private bool AutenticaUsuario(String path, String user, String pass)
+        {
+            //Los datos que hemos pasado los 'convertimos' en una entrada de Active Directory para hacer la consulta
+            DirectoryEntry de = new DirectoryEntry(path, user, pass, AuthenticationTypes.Secure);
+            try
+            {
+                //Inicia el chequeo con las credenciales que le hemos pasado
+                //Si devuelve algo significa que ha autenticado las credenciales
+                DirectorySearcher ds = new DirectorySearcher(de);
+                var ccc = ds.FindOne();
+                return true;
+            }
+            catch
+            {
+                //Si no devuelve nada es que no ha podido autenticar las credenciales
+                //ya sea porque no existe el usuario o por que no son correctas
+                return false;
+            }
         }
 
         public async Task<IActionResult> Logout()
